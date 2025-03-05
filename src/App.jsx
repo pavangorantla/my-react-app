29f270ccc06d35d1aca83152d01860b2e4c282fb
Pavan Gorantla <pavangorantla116@gmail.com>
2025-03-05 02:33:01 -0600

    Props in React JS

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,19 @@
-import Button from './Button/Button.jsx';
+//props = read-only properties that are shared between components
+//        A parent component can send data to a child component
+//        <component key=value />
+
+import Student from './Student.jsx';
 
 function App() {
-  return <Button />;
+  return (
+    <>
+      <Student name="Spongebob" age={30} isStudent={true} />
+      <Student name="Patrick" age={29} isStudent={false} />
+      <Student name="Squidward" age={50} isStudent={false} />
+      <Student name="Sandy" age={27} isStudent={true} />
+      <Student />
+    </>
+  );
 }
 
 export default App;
